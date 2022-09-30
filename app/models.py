5208296a763acee613b4b8104f0409827ad70417
zaredite <82401033+zaredite@users.noreply.github.com>
2022-09-30 13:52:54 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Symptoms(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    certificate = models.BooleanField()
+    certificate = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
